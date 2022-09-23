bd26dbc735a09d33a06583635624a1f3589118c1
Edilson Lima <edilson.lima1@hotmail.com>
2022-09-23 20:24:05 -0300

    Requisito 2 e 3

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './myContext';
 
 function Provider({ children }) {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [dataInputs, setDataInputs] = useState({ name: '', password: '' });
+
+  const contextValue = {
+    isDisabled,
+    setIsDisabled,
+    dataInputs,
+    setDataInputs,
+  };
+
   return (
-    <MyContext.Provider value>
+    <MyContext.Provider value={ contextValue }>
       {children}
     </MyContext.Provider>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import context from '../context/myContext';
 
 function Login() {
+  const {
+    isDisabled,
+    dataInputs,
+    setDataInputs,
+  } = useContext(context);
+  const { name, password } = dataInputs;
+  console.log(dataInputs);
+
   return (
-    <div>Login</div>
+    <div>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="text"
+          name="email"
+          id="email"
+          data-testid="email-input"
+          value={ name }
+          onChange={ ({ target: { value } }) => setDataInputs({ ...dataInputs,
+            name: value }) }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          name="password"
+          id="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value } }) => setDataInputs({ ...dataInputs,
+            password: value }) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ isDisabled }
+      >
+        Entrar
+      </button>
+
+    </div>
   );
 }
 
