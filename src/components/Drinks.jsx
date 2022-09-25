c3a66abccb10a157ae8332fb942da8c20312110b
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-09-25 18:05:59 -0300

    Requisitos 19, 20 e 21

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import context from '../context/myContext';
-
+//
 function Drinks() {
   const { fetchDrinks, drinksApi } = useContext(context);
   const [categories, setCategories] = useState([]);
