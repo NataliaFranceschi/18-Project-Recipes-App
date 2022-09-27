42c75e5f6b26e20b28ba55995838f675519fafcc
Diogo Sales <diogosales.ads@gmail.com>
2022-09-27 10:25:36 -0300

    [group-24]-req16-18

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../style/footer.css';
+
+function Footer() {
+  return (
+    <footer data-testid="footer" className="Footer">
+      <Link to="/drinks">
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="Drink" />
+      </Link>
+      <Link to="/meals">
+        <img data-testid="meals-bottom-btn" src={ mealIcon } alt="Meal" />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
