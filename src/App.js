62ac049b4e72d5df50709d75390e803eb43cde8a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-03 17:19:15 -0300

    teste Recipe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
@@ -14,20 +14,18 @@ import Recipes from './pages/Recipes';
 function App() {
   return (
     <Provider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/meals" component={ Recipes } />
-          <Route exact path="/drinks" component={ Recipes } />
-          <Route exact path="/meals/:id" component={ RecipeDetails } />
-          <Route exact path="/drinks/:id" component={ RecipeDetails } />
-          <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
-          <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/done-recipes" component={ DoneRecipes } />
-          <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Recipes } />
+        <Route exact path="/drinks" component={ Recipes } />
+        <Route exact path="/meals/:id" component={ RecipeDetails } />
+        <Route exact path="/drinks/:id" component={ RecipeDetails } />
+        <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
+        <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
     </Provider>
 
   );
--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../utils/renderWithRouter';
 import Meals from '../components/Meals';
 import Drinks from '../components/Drinks';
-import Recipes from '../pages/Recipes';
-import { testIds, VALID_EMAIL, VALID_PASSWORD } from '../utils/constants';
 import App from '../App';
 import Provider from '../context/myProvider';
 
@@ -53,31 +50,9 @@ describe('Testa a tela pricipal', () => {
     expect(button.length).toBe(6);
   });
   it('Verifica ', async () => {
-    const { history } = renderWithRouter(
-      <Provider>
-        <App />
-      </Provider>,
-    );
+    const { history } = renderWithRouter(<App />);
     history.push('/drinks');
     expect(history.location.pathname).toBe('/drinks');
-    await waitFor(screen.getByText('Drinks')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText('Drinks')).toBeInTheDocument());
   });
-  /* it('Verifica ', async () => {
-    const { history } = renderWithRouter(<App />);
-
-    const email = screen.getByTestId(testIds.emailInput);
-    const buttao = screen.getByTestId(testIds.loginSubmitButton);
-    const password = screen.getByTestId(testIds.passwordInput);
-    expect(buttao).toBeDisabled();
-    userEvent.type(email, VALID_EMAIL);
-    userEvent.type(password, VALID_PASSWORD);
-    expect(email).toHaveValue(VALID_EMAIL);
-    expect(password).toHaveValue(VALID_PASSWORD);
-    userEvent.click(buttao);
-    await waitFor(() => expect(screen.getByText('Meals')).toBeInTheDocument(), { timeout: 3000 });
-    const drink = screen.getByTestId(/drinks-bottom-btn/i);
-    userEvent.click(drink);
-    await waitFor(() => expect(screen.getByText('Drinks')).toBeInTheDocument(), { timeout: 3000 });
-    expect(history.location.pathname).toBe('/drink');
-  }); */
 });
